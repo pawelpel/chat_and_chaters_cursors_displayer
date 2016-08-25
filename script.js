34bade86b435200b87ab4e6709756f4ad0aa13d3
Paweł <pawelpel@MacBook-Air-Pawel.local>
2016-08-25 17:24:14 +0200

    all works

--- script.js
+++ script.js
@@ -1,20 +1,157 @@
 document.addEventListener('DOMContentLoaded', function(){
 
-    var ws = new WebSocket("ws://localhost:8888/ws");
-    
-    ws.onopen = function() {
-       ws.send("Hello, world");
-    };
-    
-    
-    document.querySelector('body').onmousemove = function(e){
-        ws.send(e.clientX + ' ' + e.clientY);
+    function MouseTogether(){
+        if(!(this instanceof MouseTogether)) return new MouseTogether();
+        
+        this.usernameButton = document.querySelector('input[name=submit]');
+        this.usernameInput = document.querySelector('input[name=username]');
+        this.username = '';
+        
+        this.joined = false;
+        this.id = 'img_'+Math.round(Math.random()*1000000) + '';
+        
+        this.message = document.querySelector('input[name=message]');
+        this.messageSendButton = document.querySelector('input[name=sendmessage]');
+        
+        this.activityBox = document.querySelector('#activity');
+        
+        this.playerCounter = document.querySelector('#counter');
+        
+        this.body = document.querySelector('body');
+        
+        this.init();
+        this.onMessage();
+        this.onClose();
     }
     
     
-    ws.onmessage = function (evt) {
-       document.querySelector('h1').innerHTML = evt.data;
-    };
+    MouseTogether.prototype.init = function(){
 
+        this.ws = new WebSocket("ws://192.168.0.100:8888/ws");
+        
+        this.usernameButton.onclick = function(e){      
+            e.preventDefault();
+        
+            this.usernameButton .setAttribute('disabled', 'disabled');
+            this.usernameInput.setAttribute('readonly', 'readonly');
+            this.username = this.usernameInput.value;
+        
+            this.messageSendButton.removeAttribute('disabled');
+            this.onOpen(this.username);
+            this.joined = true;
+            
+        }.bind(this);
+        
+        this.messageSendButton.onclick = function(e){
+            e.preventDefault();
+            
+            this.sendData({
+                type: 'message',
+                name: this.username,
+                message: this.message.value
+            })
+            
+            this.message.value = "";
+        }.bind(this);
+        
+        this.body.onmousemove = function(e){
+            if(this.joined){
+                this.sendData({
+                    type: 'mouse',
+                    x: e.clientX,
+                    y: e.clientY,
+                    id: this.id
+                })
+            }
+        }.bind(this);
+    }
+    
+    
+    MouseTogether.prototype.onOpen = function(username){
+        
+        this.ws.onopen = function() {
+            console.log('Connection opened');
+            
+            this.sendData({
+                type: 'status',
+                value: 'joined',
+                name: username,
+            })
+        }.bind(this);
+        
+        this.ws.onopen();
+    }
+    
+    
+    MouseTogether.prototype.onMessage = function(){
+        
+        this.ws.onmessage = function(e){
+            var answer = JSON.parse(e.data);
+            
+            if(answer['id']){
+                if(answer['id'] !== this.id){
+                    var cursor = document.getElementById(answer['id']);
+                    
+                    if(cursor === null){
+                        var newCursor = document.createElement('img');
+                        newCursor.setAttribute('id', answer['id']);
+                        newCursor.classList.add('cursors');
+                        newCursor.setAttribute('src', 'cursor.png');
+                        this.body.appendChild(newCursor);
+                    } else {
+                        cursor.style = "left: "+answer['x']+"px; top: "+answer['y']+'px';   
+                    }
+                }
+            } else {
+                this.renderRow(answer);
+            }
+            
+            this.playerCounter.innerHTML = answer['counter'];
+            
+        }.bind(this);
+    }
+
+    
+    MouseTogether.prototype.renderRow = function(data){
+        
+        var span = document.createElement('span');
+        
+        span.classList.add('message')
+        
+        if(data['type'] === 'message'){
+            span.innerHTML = '<strong>'+data['name']+': </strong>'+data['message'];
+            span.innerHTML += '<br>';
+            this.activityBox.appendChild(span);
+            
+        } else if (data['type'] === 'status'){
+            
+            span.classList.add('status');
+            span.innerHTML = '<strong>'+data['name']+'</strong> has '+data['value'];
+            span.innerHTML += '<br>';
+            this.activityBox.appendChild(span);
+        }
+    }
+    
+    
+    MouseTogether.prototype.onClose = function(){
+        
+        this.ws.onclose = function(){
+            console.log('Connection closed');
+            
+            this.sendData({
+                type: 'status',
+                value: 'left',
+                name: username,
+            })
+        }.bind(this);
+    }
+    
+    
+    MouseTogether.prototype.sendData = function(data){
+        
+        this.ws.send(JSON.stringify(data))
+    }
+
+    var mousetogether = MouseTogether();
     
 }, false);
--- server.py
+++ server.py
@@ -1,30 +1,71 @@
 from tornado import websocket, web, ioloop
 import json
+import os
 
 
 clients = []
 
+class IndexHandler(web.RequestHandler):
+    def get(self):
+        self.render("index.html")
+
+
 class SocketHandler(websocket.WebSocketHandler):
+
     def check_origin(self, origin):
         return True
 
     def open(self):
         print('Websocket opened!')
+
         if self not in clients:
             clients.append(self)
 
     def on_close(self):
         print('Websocket closed!')
+
+        try:
+            username = str(self.username)
+
+            for c in clients:
+                if c != self:
+                    c.write_message({'type':'status',
+                                    'value':'left',
+                                     'name':username,
+                                  'counter':str(len(clients)-1)})
+        except:
+            pass
+
         if self in clients:
             clients.remove(self)
 
+
     def on_message(self, message):
-        self.write_message('Server got: ' + message)
+
+        message = json.loads(message)
+
+
+        if 'value' in message.keys() and message['value'] == 'joined':
+            self.username = message['name']
+
+        message['counter'] = str(len(clients))
+
+
+        message = json.dumps(message)
+
+        for c in clients:
+            c.write_message(message)
 
 
 
-app = web.Application([(r'/ws', SocketHandler)])
+app = web.Application([(r'/ws', SocketHandler),
+                       (r'/', IndexHandler),
+                       (r'/(.*)', web.StaticFileHandler, {'path': os.getcwd()})])
 
 if __name__ == "__main__":
-    app.listen(8888)
-    ioloop.IOLoop.instance().start()
+    try:
+        app.listen(8888)
+        ioloop.IOLoop.instance().start()
+    except KeyboardInterrupt:
+        pass
+
